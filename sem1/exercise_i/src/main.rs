bfbefcbbf1a5afd8dc9609ce286e7584ad7336a5
mathrews <mateusps4gg@gmail.com>
2024-06-28 12:17:16 -0300

    meu amigo me ensinou a usar HashSet

--- sem1/exercise_i/src/main.rs
+++ sem1/exercise_i/src/main.rs
@@ -1,32 +1,16 @@
-use std::io;
+use std::{collections::HashSet, io};
 
 fn main() {
     let mut line1 = String::new();
     io::stdin().read_line(&mut line1).unwrap();
     let line1 = line1.trim().parse::<i32>().unwrap();
     
-    let mut values: Vec<i32> = Vec::new();
+    let mut values: HashSet<i32> = HashSet::new();
     for _ in 0..line1 {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        values.push(input.trim().parse::<i32>().unwrap_or_default());
+        values.insert(input.trim().parse::<i32>().unwrap_or_default());
     }
 
-    values.sort();
-    
-    let mut students = 0;
-    for (index, item) in values.iter().enumerate() {
-        if index == 0 {
-            students += 1;
-            continue;
-        } else {
-            if values[index] != values[index - 1] {
-                students += 1;
-            } else {
-                continue;
-            }
-        }
-    }
-
-    println!("{}", students);
+    println!("{}", values.len());
 }
