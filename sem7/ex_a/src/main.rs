34aac711baf1996748a4d33851871ca81b6c7800
mathrews <mateusps4gg@gmail.com>
2024-07-09 21:50:48 -0300

    ex_a sem7

--- sem7/ex_a/src/main.rs
+++ sem7/ex_a/src/main.rs
@@ -1,5 +1,17 @@
 fn reverse_string(s: &mut Vec<char>) {
+    let mut temp = 'a';
+    let mut start = 0;
+    let mut end = s.len() - 1;
 
+    while start < end {
+        temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start += 1;
+        end -= 1;
+    }
+
+    println!("{:?}", s);
 }
 
 fn main() {
